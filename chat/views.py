a3df9e7253a71b718b306e38793fbff9557033bc
Lesya Pany <panylessyat@gmail.com>
2022-06-14 20:53:49 +0100

    added the first new game view

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+import random
+from models import *
+from rest_framework.views import APIView
+from serializers import *
+from rest_framework.response import Response
+
 
 
 def index(request):
@@ -9,3 +15,25 @@ def room(request, room_name):
     return render(request, 'chat/room.html', {
         'room_name': room_name
     })
+
+class NewGameView(APIView):
+    def post(self, request):
+        print('waiting class 2')
+        try:
+            letter = random.choice(['P', 'B', 'K', 'S'])
+            category = random.choice(Category.objects.all())
+            task = Task.objects.create(letter=letter, category=category)
+
+            print(request.data)
+            
+            user_id = request.data['user_id']
+            print(user_id)
+            user = Profile.objects.get(user=user_id)
+
+            game = Game.objects.create(task=task, player_1=user)
+            game_data = GameSerializer(game).data
+            return Response(game_data, status=201)
+
+        except Exception as e:
+            print(e)
+            return Response({'error': str(e)}, status=500)
