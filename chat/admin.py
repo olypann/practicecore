2781a5c87cbcec7dc7fa7c196ffff2f99a6f187a
Lesya Pany <panylessyat@gmail.com>
2022-06-14 20:40:42 +0100

    added models, admin, serializers

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import *
+
+
+admin.site.register(Profile)
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(Word)
+admin.site.register(Game)
+admin.site.register(GameWord)
