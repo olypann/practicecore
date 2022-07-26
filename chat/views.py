d6213ac434438ed4d62dde79c33499f4acd7304b
Lesya Pany <panylessyat@gmail.com>
2022-07-26 09:23:48 +0100

    -

--- chat/views.py
+++ chat/views.py
@@ -44,3 +44,75 @@ class NewGameView(APIView):
         except Exception as e:
             print(e)
             return Response({'error': str(e)}, status=500)
+
+
+
+
+class GameView(APIView):
+    #Создаем функцию get которая принимает на вход id игры и контекст в виде request и с помощью этого достает слова для каждого игрока
+    def get(self, request, game_id):
+        print('проверка апи')
+        try:
+            # game_id = request.query_params.get('game_id')
+            # создаем список с одним элементом игры, так как только у одной может быть подходящий id
+            game = Game.objects.filter(id=game_id)
+            #Если в этом списке есть игра(если такая игра нашлась)
+            if game:
+                print(game[0].player_1, game[0].player_2)
+                
+
+
+                #Создаем переменную serializer которая достает данные о нужной нам игре в правильном формате
+                serializer = GameSerializer(game[0], context={'request': request})
+                # print(serializer.data)
+                #Сначала создаем список со словами которые находятся в правильной игре и у нужно нам игрока
+                words_1 = GameWord.objects.filter(game=game[0], player=game[0].player_1)
+                #Мы создаем пустой список который будет содержать только текстовый вид самого слова без дополнительной информации
+                words_1_list = []
+                for w in words_1:
+                    words_1_list.append(w.word.word)
+
+                #Повторяем процесс для списка слов второго игрока
+                words_2 = GameWord.objects.filter(game=game[0], player=game[0].player_2)
+                words_2_list = []
+                for w in words_2:
+                    words_2_list.append(w.word.word)
+
+                #Создаем переменную response которая будет содержать все данные из ранее полученных данных об игре(задание и оба игрока)
+                response = serializer.data
+                #После этого добавляем полученные списки слов к данным об игре
+                response['words_1'] = words_1_list
+                response['words_2'] = words_2_list
+                #Возвращаем response который позже будет отрисован функцией game_view
+                return Response(response)
+
+            #Если такая игра не найдена, выводим ошибку об этой ситуации
+            return Response({'status': 'error',
+                             'error': 'game with id {} not found'.format(game_id)})
+        #В случае ошибки возвращаем текст который сообщает о проблеме
+        except Exception as e:
+            return Response({'status': 'error',
+                             'error': str(e)})
+
+    def put(self, request, game_id):
+        print('helloworldagain')
+        game = Game.objects.filter(id=game_id)
+        if game and game[0].player_1 is not None and game[0].player_2 is None:
+            game = game[0]
+            game.status = 'A'
+            
+            user_id = request.data['user_id']
+            user = Profile.objects.get(user=user_id)
+            game.player_2 = user
+
+            game.end_time = now()+datetime.timedelta(0, game.task.time)
+            game.save()
+
+            serializer = GameSerializer(game)
+            data = serializer.data
+            print(data)
+            return Response(data, status=200)
+        
+        else:
+            return Response({'status': 'error',
+                             'error': 'There was an error oops'})
