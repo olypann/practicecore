d9b46c0610eb9c2f0d9e89b9da40ec3d152c692d
Lesya Pany <panylessyat@gmail.com>
2022-09-05 15:25:38 +0100

    -

--- core/settings.py
+++ core/settings.py
@@ -136,7 +136,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, '../dashboard/static/dashboard/css'),
+]
 
 
 ASGI_APPLICATION = "core.routing.application"
