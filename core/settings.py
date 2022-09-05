2236545c9f5276f4a639753f183a1d493687f328
Lesya Pany <panylessyat@gmail.com>
2022-09-05 15:29:31 +0100

    -

--- core/settings.py
+++ core/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'staticfiles'),
-    os.path.join(BASE_DIR, '../dashboard/static/dashboard/css'),
+    os.path.join(BASE_DIR, 'static_root'),
+    # os.path.join(PROJECT_DIR, 'static'),
 ]
 
 
